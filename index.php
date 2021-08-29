1a0a5768bc5160d316b3ec0e9b855bc859b5dddf
Rolf <rolf@yoast.com>
2021-08-29 12:33:12 +0200

    Make beginnings of overview for players

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
-
-use frontier\ploball\database\Database_Connection;
-
-require __DIR__ . './vendor/autoload.php';
-
-$blaat = new Database_Connection();
-
-$blaat->connect();
+	require './includes/admin_includes.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Talon</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="./assets/css/style.min.css" />
+</head>
+<body>
+	<header>
+		<div class="row">
+			<picture>
+				<img class="logo" src="./assets/images/talon.svg" />
+			</picture>
+			<h1>
+				T.A.L.O.N.
+			</h1>
+		</div>
+	</header>
+</body>
+</html>
