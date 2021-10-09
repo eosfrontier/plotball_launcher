61d3971ba28f6e5dac9fe3024f8115e9ab832fbe
Rolf <rolf@yoast.com>
2021-10-10 01:24:14 +0200

    Fix joomla include

--- includes/admin_includes.php
+++ includes/admin_includes.php
@@ -11,7 +11,7 @@ $dotenv   = Dotenv::createImmutable( $root_dir );
 $dotenv->load();
 
 if ( isset( $_ENV['SERVER'] ) && ( $_ENV['SERVER'] === 'production' ) ) {
-	require './joomla.php';
+	require './../includes/joomla.php';
 
 	if ( in_array( '30', $jgroups ) || in_array( '8', $jgroups ) || in_array( '7', $jgroups ) || in_array( '36', $jgroups ) ) {
 		$access = true;
